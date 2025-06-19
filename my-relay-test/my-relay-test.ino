a50aba6bb184c779e56a4cf9301aed10b3cba0b0
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2025-06-19 13:31:32 -0500

    LOW turns on relay

--- my-relay-test/my-relay-test.ino
+++ my-relay-test/my-relay-test.ino
@@ -1,14 +1,20 @@
 void setup() {
   // Initialize digital pin 3 as an output
+  Serial.begin(9600);
   pinMode(3, OUTPUT);
+  Serial.println("\n================== start ================");
+  digitalWrite(3, LOW);
+  Serial.println("Relay on");
 }
 
 void loop() {
-  // Turn pin 3 HIGH for 2 seconds
-  digitalWrite(3, HIGH);
-  delay(5000);
+  // // Turn pin 3 HIGH for 2 seconds
+  // digitalWrite(3, HIGH);
+  // Serial.println("Relay off");
+  // delay(5000);
   
-  // Turn pin 3 LOW for 2 seconds
-  digitalWrite(3, LOW);
-  delay(5000);
+  // // Turn pin 3 LOW for 2 seconds
+  // digitalWrite(3, LOW);
+  // Serial.println("Relay on");
+  // delay(5000);
 }
